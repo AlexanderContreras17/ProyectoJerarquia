6c05cd1fc1538956eb28d72b8d41d1f142cd7071
Alexander Contreras <102634173+AlexanderContreras17@users.noreply.github.com>
2024-06-19 20:02:01 -0600

    Delete udp-wpf-server directory

--- udp-wpf-server/Models/Alumno.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace udp_wpf_server.Models;
-
-public class Alumno
-{
-    public string Nombre { get; set; } = null!;
-}
--- udp-wpf-server/Models/DTOs/InscribeDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace udp_wpf_server.Models.DTOs;
-
-public class InscribeDto
-{
-    public string Nombre { get; set; } = null!;
-    public string Taller { get; set; } = null!;
-}
-
