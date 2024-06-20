9a4f4efec415f36eb1cd424f5db132b38325b5a6
Alexander Contreras <102634173+AlexanderContreras17@users.noreply.github.com>
2024-06-19 20:04:11 -0600

    Delete tcp-wpf-client directory

--- tcp-wpf-client/Models/DTOs/MessageDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace tcp_wpf_client.Models.DTOs;
-
-public class MessageDto
-{
-    public string Mensaje { get; set; } = null!;
-    public string Origen { get; set; } = null!;
-    public DateTime Fecha { get; set; }
-}
