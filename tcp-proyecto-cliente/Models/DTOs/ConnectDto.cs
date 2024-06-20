bdb5e49868ade0a7949644eef2052c12da8ff64a
Alexander Contreras <102634173+AlexanderContreras17@users.noreply.github.com>
2024-06-19 20:04:42 -0600

    Delete tcp-proyecto-cliente directory

--- tcp-proyecto-cliente/Models/DTOs/ConnectDto.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace tcp_proyecto_cliente.Models.DTOs;
-
-public class ConnectDto
-{
-    public string Name { get; set; } = null!;
-    public string Message { get; set; } = null!;
-}
--- tcp-proyecto-cliente/Models/DTOs/PictureDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace tcp_proyecto_cliente.Models.DTOs;
-
-public class PictureDto
-{
-    public string Autor { get; set; } = null!;
-    public string Subject { get; set; } = null!;
-    public string Image { get; set; } = null!;
-    public DateTime Date { get; set; }
-}
